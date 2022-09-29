f55ecd719a47218d6348c1eb4f5b8a88361abc33
1Nexts <game1nexts@gmail.com>
2022-09-29 20:30:33 +0700

    Ticketlist edit section id + block margin

--- pages/ticket-list/index.tsx
+++ pages/ticket-list/index.tsx
@@ -17,8 +17,10 @@ export default function TicketList() {
       <HeaderMenu></HeaderMenu>
 
       <div id="wrapper">
-        <section id={styles.header}>
+
+        <section id={styles["section-ticket-list"]}>
           <div className={styles.block}>
+
             <div className={"input-group " + styles["search-group"]}>
               <span className="input-group-text" id="basic-addon1">
                 <img src="/static/search.svg" />
@@ -29,18 +31,17 @@ export default function TicketList() {
                 placeholder="Search ticket"
               />
             </div>
-          </div>
-        </section>
 
-        <section id={styles.content}>
-          <div className={styles.block}>
-            <div className={"row " + styles["content"]}>
-              {[1, 2, 3, 4].map((value) => {
-                return (
-                <TicketItemComponent key={value}></TicketItemComponent>
-                );
-              })}
+            <div className={styles["block-content"]}>
+              <div className={"row m-0 " + styles["content"]}>
+                {[1, 2, 3, 4].map((value) => {
+                  return (
+                    <TicketItemComponent key={value}></TicketItemComponent>
+                  );
+                })}
+              </div>
             </div>
+
           </div>
         </section>
       </div>
