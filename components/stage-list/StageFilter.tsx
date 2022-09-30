2e9d1430219d8ce99132cba2f132b3b5d0952b33
1Nexts <game1nexts@gmail.com>
2022-09-30 12:47:53 +0700

    StageFilter UI add filter  low price, best seat

--- components/stage-list/StageFilter.tsx
+++ components/stage-list/StageFilter.tsx
@@ -22,7 +22,7 @@ const StageFilterComponent = ({}: CardProps) => {
       {/* <select className={styles["select-ticket"]} aria-label="Quantity">
   
       </select> */}
-      <select className={"form-select "+styles['select-ticket']}>
+      <select className={"form-select " + styles["select-ticket"]}>
         <option value={1}>1 Ticket</option>
         <option value={2}>2 Tickets</option>
         <option value={3}>3 Tickets</option>
@@ -35,34 +35,24 @@ const StageFilterComponent = ({}: CardProps) => {
         <option value={8}>10 Tickets</option>
       </select>
 
-      <div className={styles["slidecontainer"]}>
+      <div className={styles["filter-price"]}>
         <div className={"row m-0"}>
-          <div className={"col-3 "}>
-            <h4>
-              $<span id="demo" />
-            </h4>
+          <hr />
+          <div className={"col-6 " + styles["price-selected"]}>
+            <button type="button" className="btn">
+              Low Price
+            </button>
           </div>
 
-          <div className={"col-6 "+styles['slider-padding']}>
-            <input
-              type="range"
-              min={18}
-              max={1000}
-              defaultValue={120}
-              className={styles["slider"]}
-              id="myRange"
-              step="50"
-            />
+          <div className={"col-6 "}>
+            <button type="button" className="btn">
+              Best Seats
+            </button>
           </div>
 
-          <div className={"col-3 "}>
-            <h4>
-              $1000
-            </h4>
-          </div>
+          <hr />
         </div>
       </div>
-   
     </section>
   );
 };
