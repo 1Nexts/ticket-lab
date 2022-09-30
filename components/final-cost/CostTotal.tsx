08f5dba78afa959b8417373d8704f2cca56b46b9
1Nexts <game1nexts@gmail.com>
2022-09-30 16:02:41 +0700

    UI TotalComponent

--- components/final-cost/CostTotal.tsx
+++ components/final-cost/CostTotal.tsx
@@ -1,24 +1,95 @@
 import React from "react";
 import styles from "../../styles/final-cost/components/costTotal.module.scss";
-type CardProps = {
-
-};
-const FinalCostTotalComponent = ({  }: CardProps) => {
+type CardProps = {};
+const FinalCostTotalComponent = ({}: CardProps) => {
+  const ticketPrice = 76.0;
+  const amountTicket = 2;
+  const SERVICE_FEE_PERCENT = 0.1;
 
   return (
     <section id={styles["cost-total"]}>
       <div className={styles["block-total"]}>
-        <h4>
-          Total   $999
-        </h4>
-        <h5>Mobile Entry - Free</h5>
+        {/* Total */}
+        <div className={"row m-0 " + styles["item-row"]}>
+          <div className={"col-9 p-0 " + styles["col-1"]}>
+            <h4>Total</h4>
+          </div>
 
-        <h6>
-          Tickets Avaliable by Sun Oct 8, 2022 These mobile tickets will be
-          transferred directly to you from a trusted seller,We'll email you
-          instructions on how to accept them on the original ticket provider's
-          mobile app.
-        </h6>
+          <div className={"col-3 p-0 " + styles["col-2"]}>
+            <h4>$999</h4>
+          </div>
+        </div>
+        <br />
+
+        {/* Tickets */}
+        <h5>Tickets</h5>
+        <div className={"row m-0 " + styles["item-row"]}>
+          <div className={"col-9 p-0 " + styles["col-1"]}>
+            <h6>
+              Resale Tickets: ${ticketPrice.toFixed(2)} x {amountTicket}
+            </h6>
+          </div>
+
+          <div className={"col-3 p-0 " + styles["col-2"]}>
+            <h6>${(ticketPrice * amountTicket).toFixed(2)}</h6>
+          </div>
+        </div>
+
+        {/* Fee */}
+        <h5>Fee</h5>
+        <div className={"row m-0 " + styles["item-row"]}>
+          <div className={"col-9 p-0 " + styles["col-1"]}>
+            <h6>
+              Service Fee: ${(ticketPrice * SERVICE_FEE_PERCENT).toFixed(2)} x{" "}
+              {amountTicket}
+            </h6>
+          </div>
+
+          <div className={"col-3 p-0 " + styles["col-2"]}>
+            <h6>
+              ${(ticketPrice * SERVICE_FEE_PERCENT * amountTicket).toFixed(2)}
+            </h6>
+          </div>
+        </div>
+
+        {/* Delevery */}
+        <h5>Delevery</h5>
+        <div className={"row m-0 " + styles["item-row"]}>
+          <div className={"col-9 p-0 " + styles["col-1"]}>
+            <h6>Mobile Entry</h6>
+          </div>
+
+          <div className={"col-3 p-0 " + styles["col-2"]}>
+            <h6>Free</h6>
+          </div>
+        </div>
+
+        <br />
+        <h5 className="text-primary stretched-link">Cancle order</h5>
+        <br />
+        <h6 className="text-black">All Sales Final - No Refunds</h6>
+
+        <div className="form-check">
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value=""
+            id="flexCheckChecked"
+            defaultChecked
+          />
+          <label className="form-check-label" htmlFor="flexCheckChecked">
+            I Have read and agree to current{" "}
+            <span className="text-primary stretched-link">Terms of Use.</span>
+          </label>
+        </div>
+
+        <br />
+        <button
+          type="button"
+          className={"btn btn-success " + styles["btn-place-order"]}
+        >
+          Place Order
+        </button>
       </div>
     </section>
   );
