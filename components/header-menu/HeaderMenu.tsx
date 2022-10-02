1492474232544d1df52a2b9fa29fc5302b9878b3
1Nexts <game1nexts@gmail.com>
2022-10-03 01:05:10 +0700

    Header Menu - add more mock menu

--- components/header-menu/HeaderMenu.tsx
+++ components/header-menu/HeaderMenu.tsx
@@ -2,40 +2,50 @@ import Link from "next/link";
 import React from "react";
 import styles from "./header.module.scss";
 
-
-type CardProps = {
-
-};
-const HeaderMenu = ({  }: CardProps) => {
-
+type CardProps = {};
+const HeaderMenu = ({}: CardProps) => {
   return (
     <section id={styles["wrapper-header"]}>
-        <div className={styles["header-block"]}>
-          <div>
-            <ul className="navbar-nav flex-row flex-wrap text-white">
-              <li className={"nav-item "+styles['logo']}>
-                <h2 className="text-white font-weight-bold m-0 pe-3">ticketlab</h2>
-              </li>
-        
-              <li className="nav-item">
-                <Link href="/concert-list/ed-sheeran">
-                  <a className="nav-link py-2 px-3">Concerts</a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/stage-list">
-                  <a className="nav-link py-2 px-3">Stage</a>
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link href="/final-cost">
-                  <a className="nav-link py-2 px-3">Cost</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
+      <div className={styles["header-block"]}>
+        <div>
+          <ul className="navbar-nav flex-row flex-wrap text-white">
+            <li className={"nav-item " + styles["logo"]}>
+              <h2 className="text-white font-weight-bold m-0 pe-3">
+                ticketlab
+              </h2>
+            </li>
+
+            <li className="nav-item">
+              <Link href="/concert-list/ed-sheeran">
+                <a className="nav-link py-2 px-3">Concerts</a>
+              </Link>
+            </li>
+            {/* <li className="nav-item">
+              <Link href="/stage-list">
+                <a className="nav-link py-2 px-3">Stage</a>
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link href="/final-cost">
+                <a className="nav-link py-2 px-3">Cost</a>
+              </Link>
+            </li> */}
+
+            <li className="nav-item">
+                <a className="nav-link py-2 px-3">Sports</a>
+            </li>
+
+            <li className="nav-item">
+                <a className="nav-link py-2 px-3">Art & Theater</a>
+            </li>
+
+            <li className="nav-item">
+                <a className="nav-link py-2 px-3">More</a>
+            </li>
+          </ul>
         </div>
-      </section>
+      </div>
+    </section>
   );
 };
 
