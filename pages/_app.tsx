8e296f54d90eea41da26a818f9cf2bb9005e13b8
1Nexts <game1nexts@gmail.com>
2022-10-07 18:38:16 +0700

    add libary react-redux

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "../styles/globals.css";
 
 import { Provider } from "react-redux";
